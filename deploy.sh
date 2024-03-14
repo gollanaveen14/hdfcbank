c6f0f678ef8c91254fb206902b5793f7c645ce0f
gollanaveen14 <gollanaveen793@gmail.com>
2024-03-14 06:17:57 +0000

    new code

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,1 @@
-this script contain the deployment script
-again updated
-added to dev branch
-this is the development branch
-
-this is line is for stash command purpose
-
-this line is for cherrypickcommand
+new files in deploy
